190e8a8adbff5f513f10a0f04c2548d8c722dca5
Vrinda Bhatia <vrindabhatia999@gmail.com>
2020-12-08 18:43:58 +0530

    commit

--- login.js
+++ login.js
@@ -1,22 +1,28 @@
 function validate(){
     var username=document.getElementById("uname");
     var pass=document.getElementById('pass').value;
+    var ul=document.getElementById("ul");
+    var pl=document.getElementById('pl')
 
      if(username.value.trim()==""){
          alert("Blank username");
          username.style.border="solid 2px red";
+         ul.style.visibility="visible"
          return false;
      }
      else if(pass.trim()==""){
          alert("password cant be blank")
+         pl.style.visibility="visible"
          return false;
      }
      else if(pass.trim().length<5){
          alert("password too short")
          return false;
      }
+     
      else{
          username.style.border="solid 2px green"
+         pl.style.visibility="hidden"
          return true;
      }
 
