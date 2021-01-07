170f50067c27583d5307f7400f0e23472897f7dd
Vrinda Bhatia <vrindabhatia999@gmail.com>
2021-01-07 20:06:47 +0530

    commit

--- Jquery.js
+++ Jquery.js
@@ -16,3 +16,17 @@ $( ".change1" ).on("click", function() {
         $( ".change1" ).text( "ON" ); 
     } 
 }); 
+
+
+setInterval(
+    function () {
+      var j1=document.getElementById("j1");
+    
+      j1.style.backgroundColor = "#"+ ((1<<24)*(Math.random()+3)|0).toString(16).substr(1);
+    },2000);
+  
+    var j2=document.getElementById("j2");
+    j2.style.backgroundColor="#305354"
+    j2.style.alignContent="center"
+    j2.style.alignItems="center"
+    j2.style.justifyContent="center"
